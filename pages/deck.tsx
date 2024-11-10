11586a21cb0cf0432b5ac5251cb533cc86e14083
Adrian Miguel <adriantmiguel@gmail.com>
2024-11-10 12:33:26 -0800

    using grid

--- pages/deck.tsx
+++ pages/deck.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FC } from 'react';
-import { Button, Grid } from '@mui/material';
+import { Button, Grid2 as Grid } from '@mui/material';
 import { PageWrapper } from '../components/pageWrapper';
 import { STANDARD_DECK } from '../services/deck/types';
 import Link from 'next/link';
@@ -9,26 +9,22 @@ import Link from 'next/link';
 const Card: FC = () => {
   return (
     <PageWrapper>
-      <Grid container spacing={2} columns={16}>
-        <Grid size={8}>
-          <Item>size=8</Item>
-        </Grid>
-        <Grid size={8}>
-          <Item>size=8</Item>
-        </Grid>
+      <Grid container spacing={2} columns={13}>
+        {STANDARD_DECK.map((card, index) => (
+          <Grid size={1} key={index}>
+            <Button>
+              <Link
+                href={{
+                  pathname: '/card',
+                  query: { rank: card.rank, suit: card.suit },
+                }}
+              >
+                {card.rank} of {card.suit}
+              </Link>
+            </Button>
+          </Grid>
+        ))}
       </Grid>
-      {STANDARD_DECK.map((card, index) => (
-        <Button key={index}>
-          <Link
-            href={{
-              pathname: '/card',
-              query: { rank: card.rank, suit: card.suit },
-            }}
-          >
-            {card.rank} of {card.suit}
-          </Link>
-        </Button>
-      ))}
     </PageWrapper>
   );
 };
