035e4e2c6f93be568eeb379967a44405051e60db
Adrian Miguel <adriantmiguel@gmail.com>
2024-11-10 12:30:05 -0800

    upgrading mui

--- pages/deck.tsx
+++ pages/deck.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FC } from 'react';
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { PageWrapper } from '../components/pageWrapper';
 import { STANDARD_DECK } from '../services/deck/types';
 import Link from 'next/link';
@@ -9,6 +9,14 @@ import Link from 'next/link';
 const Card: FC = () => {
   return (
     <PageWrapper>
+      <Grid container spacing={2} columns={16}>
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+      </Grid>
       {STANDARD_DECK.map((card, index) => (
         <Button key={index}>
           <Link
