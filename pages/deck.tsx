47af531843b7675ce366e8a3010a547e1a5fe8c7
Adrian Miguel <adriantmiguel@gmail.com>
2024-11-10 12:23:52 -0800

    saving working links

--- pages/deck.tsx
+++ pages/deck.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
 
 import { FC } from 'react';
+import { Button } from '@mui/material';
 import { PageWrapper } from '../components/pageWrapper';
 import { STANDARD_DECK } from '../services/deck/types';
 import Link from 'next/link';
 
 const Card: FC = () => {
   return (
-    <>
-      <PageWrapper>
-        {STANDARD_DECK.map((card, index) => (
-          <div key={index}>
-            <Link
-              legacyBehavior
-              href={{
-                pathname: '/card',
-                query: { rank: card.rank, suit: card.suit },
-              }}
-            >
-              <a>
-                {card.rank} of {card.suit}
-              </a>
-            </Link>
-          </div>
-        ))}
-      </PageWrapper>
-      <style>{`
-        a {
-          color: blue;
-          cursor: pointer;
-          text-decoration: underline;
-        }
-      `}</style>
-    </>
+    <PageWrapper>
+      {STANDARD_DECK.map((card, index) => (
+        <Button key={index}>
+          <Link
+            href={{
+              pathname: '/card',
+              query: { rank: card.rank, suit: card.suit },
+            }}
+          >
+            {card.rank} of {card.suit}
+          </Link>
+        </Button>
+      ))}
+    </PageWrapper>
   );
 };
 
